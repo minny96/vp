eb9cac0ab299b8b33f3eee3c35f95264a724490a
minihanov.r <minihanov.r@imatic.pro>
2018-03-22 10:25:29 +0300

    Bug fixes

--- sms.php
+++ sms.php
@@ -4,7 +4,7 @@
     $token = "598755174:AAHz4tJeGrhCehaNcJO6O1TFtgex-Fwl3ME";
     $chatid = "43115581";
 
-    $mess = "Новый заказ, сучка! \n <b>Имя: </b>".$name."\n<b>Телефон:</b> ".$phone;
+    $mess = "Новый заказ, сучка! \nИмя: ".$name."\nТелефон: ".$phone;
     $tbot = file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chatid."&text=".urlencode($mess));
 
 ?>
